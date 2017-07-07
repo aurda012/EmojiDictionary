e69032d5e7ee9faee336cf10d43434f97324f029
aurda012 <alfredo.i.urdaneta@gmail.com>
2017-07-07 01:12:23 -0400

    Finished app!

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Alfredo Urdaneta on 7/7/17.
+//  Copyright © 2017 AlfredosApps. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+    
+    
+    @IBOutlet weak var definitionLabel: UILabel!
+    
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var emoji = "NO EMOJI"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        definitionLabel.text = "Hello"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+   
+
+}
